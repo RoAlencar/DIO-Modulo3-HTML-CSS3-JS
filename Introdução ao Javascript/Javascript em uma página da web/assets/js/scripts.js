4d7cf15717c11a016f8329f7c99e9e7b29df6af2
RoAlencar <roalencarx@gmail.com>
2022-06-29 13:06:17 -0300

    Criando um contador com regras de menor e maior valor

--- Introdução ao Javascript/Javascript em uma página da web/assets/js/scripts.js
+++ Introdução ao Javascript/Javascript em uma página da web/assets/js/scripts.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+    if (currentNumber != 10) {
+        currentNumber++;
+        currentNumberWrapper.innerHTML = currentNumber;
+    } else {
+        console.log('Não são permitidos valores acima de 10!')
+    }
+}
+
+function decrement() {
+    if (currentNumber != 0) {
+        currentNumber--;
+        currentNumberWrapper.innerHTML = currentNumber;
+    } else {
+        console.log('Valores negativos não são permitidos!')
+    }
+}
