49906e71fadd35294076d6e1946fceff742a3f7b
RoAlencar <roalencarx@gmail.com>
2022-06-30 10:48:00 -0300

    Atribuindo valores - Variaveis e Hoisting

---
+++ JavaScript - Variáveis e Tipos/1-Hoisting.js
@@ -0,0 +1,18 @@
+numberOne = 1;
+
+console.log(numberOne + 2);
+
+var numberOne;
+
+var fistName = 'João';
+var lastName = 'Souza';
+
+if(fistName === 'João') {
+    let lastName = 'Rodrigues';
+    var fistName = 'Pedro';
+    lastName = "Silva";
+
+    console.log(lastName);
+}
+
+console.log(fistName, lastName)
---
+++ JavaScript - Variáveis e Tipos/README.md
@@ -0,0 +1,3 @@
+# JavaScript - Variáveis e Tipos
+
+Curso ministrado pela Stephany Nusch, no Santander Bootcamp Fullstack Developer de 2022 
