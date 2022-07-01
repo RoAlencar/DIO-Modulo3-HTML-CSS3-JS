f117085199af2f77f01af4168e6a442588054894
RoAlencar <roalencarx@gmail.com>
2022-07-01 20:20:54 -0300

    Reduce e sintaxe

---
+++ JavaScript - Map, Filter e Reduce/3-Reduce.js
@@ -0,0 +1,18 @@
+/**
+ * O que é o Reduce
+ * 
+ * - Executa uma função em todos os elementos do array, retornando um valor unico
+ *
+ * Sintaxe
+ * 
+ * array.reduce(callbackFn, initialValue)
+ * 
+ * - Callback: função a ser executada a partir do acumulador
+ * - initialValue (Opcional): valor osbre o qual o retorno final irá atuar   
+ */
+
+const callbackFn = function(accumulator, currentValue, index, array){
+
+}
+
+array.reduce(callbackFn, initialValue)
