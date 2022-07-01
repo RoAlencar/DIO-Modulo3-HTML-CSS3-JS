79a25a5e687c6fa129ab1e97bda19db16d8b9b6e
RoAlencar <roalencarx@gmail.com>
2022-07-01 20:08:57 -0300

    Mapping function - O que é, Sintaxe e Map vs forEach

---
+++ JavaScript - Map, Filter e Reduce/1-MapvsForEach.js
@@ -0,0 +1,18 @@
+/**
+ * O que é o Mapping function
+ * - Cria um novo array
+ * - Não modifica o original
+ * - Realiza as operações em ordem
+ */
+
+//Usando map
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array.map((item) => item * 2));
+
+//Usando forEach
+
+const array2 = [1, 2, 3, 4, 5];
+
+console.log(array.forEach((item) => item * 2))
---
+++ JavaScript - Map, Filter e Reduce/README.md
@@ -0,0 +1,3 @@
+# JavaScript - Map, Filter e Reduce
+
+Curso ministrado pela Stephany Nusch, no Santander Bootcamp Fullstack Developer de 2022 
